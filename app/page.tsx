de6d7ead3b6dde5d6c72dad335b3b6de391254cf
dorimiamn <dorimiamn@gmail.com>
2025-01-11 17:32:21 +0900

    Next.js と Tailwind CSS の導入

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
+            <h1>Hello, Next.js!</h1>
+        </div>
+    )
+  }
