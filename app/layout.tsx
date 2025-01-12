401afd2afda2f8c66735d1b64017d87135ee0612
dorimiamn <dorimiamn@gmail.com>
2025-01-13 01:43:36 +0900

    Google Fonts を利用するように変更

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,9 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <head>
+        <style>
+          @import url('https://fonts.googleapis.com/css2?family=Capriola&family=Kiwi+Maru:wght@500&display=swap');
+        </style>
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
       </head>
       <body className='min-w-96 max-w-3xl mx-auto my-10'>{children}</body>
